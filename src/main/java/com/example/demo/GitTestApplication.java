722ab58296442b7de29af256301a360d712d8873
priyalakshmi4489 <priya.lakshmi2k9@gmail.com>
2020-05-19 17:26:05 +0530

    added if condition

--- src/main/java/com/example/demo/GitTestApplication.java
+++ src/main/java/com/example/demo/GitTestApplication.java
@@ -6,15 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitTestApplication {
 
-	public static void test(){
-		System.out.println("Inside Test method");
-	}
+    public static void test() {
+        System.out.println("Inside Test method");
+        String h = "hello";
+        if (h.equals("hello")) {
+            System.out.println("Hai Hello");
+        }
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		test();
-		System.out.println("Hello GIT test !!");
-		SpringApplication.run(GitTestApplication.class, args);
-	}
+        test();
+        System.out.println("Hello GIT test !!");
+        SpringApplication.run(GitTestApplication.class, args);
+    }
 
 }
